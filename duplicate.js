17159c5e76ff5ca6a203e2a8235f76e0014c4811
unknown <rickojn@hotmail.com>
2017-12-08 20:53:12 +0000

    first commit

---
+++ duplicate.js
@@ -0,0 +1,18 @@
+let duplicates = [{state: 'NY', line: '010'}, {state: 'NY', line: '010'}, {state: 'NY', line: '011'}]
+console.log('duplicates: ', duplicates)
+
+nonDuplicates =  duplicates.filter(function (element,index,array) {
+    return array.findIndex(function(findElement){
+        return findElement.line === element.line && findElement.state === element.state
+    }) === index
+})
+
+console.log('non duplicates: ', nonDuplicates)
+
+nonDuplicates2 =  duplicates.filter(function (element,index,array) {
+    return array.findIndex(function(findElement){
+        return findElement.line === element.line && findElement.state === element.state
+    }) === index
+})
+
+console.log('non duplicates2: ', nonDuplicates2)
