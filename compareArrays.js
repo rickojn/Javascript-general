fcebedd5e8653a374541fa068952329b4b5d2317
rickojn <30386983+rickojn@users.noreply.github.com>
2018-05-03 09:26:25 +0100

    fix equal array prob

--- compareArrays.js
+++ compareArrays.js
@@ -1,11 +1,11 @@
 // let array1 = [10,20,30,50,60,120]
 // let array2 = [20,40,50,60,70,80, 90,100,110,120,130,140]
 
-// let array1 = [10,20,30,50,60,120]
-// let array2 = [20,130,40,50,60,70,80,90,100,110,120,140]
+let array1 = [10,20,30,50,60,120]
+let array2 = [20,130,40,50,60,70,80,90,100,110,120,140]
 
-let array1 = ['bob','terry','colm','brian']
-let array2 = ['william','mary','terry','bob']
+// let array1 = ['bob','terry','colm','brian']
+// let array2 = ['william','mary','terry','bob']
 
 array1.sort()
 array2.sort()
@@ -51,10 +51,19 @@ while (indexOfLarger < largerArray.length && indexOfSmaller < smallerArray.lengt
 }
 
 
-for (let i = indexOfLarger; i < largerArray.length; i++){
-    notInSmallerArray.push(largerArray[i])
+if (indexOfLarger !== largerArray.length){
+    for (let i = indexOfLarger; i < largerArray.length; i++){
+        notInSmallerArray.push(largerArray[i])
+    }
+}
+else if (indexOfSmaller !== smallerArray.length){
+    for (let i = indexOfSmaller; i < smallerArray.length; i++){
+        notInLargerArray.push(smallerArray[i])
+    }
 }
 
-
+console.log('larger array: ', largerArray)
+console.log('array 1: ', array1)
+console.log('array 2: ', array2)
 console.log('not in array 1: ', notInArray1)
 console.log('not in array 2: ', notInArray2)
