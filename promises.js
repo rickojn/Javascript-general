224f6c5da830953e4aab47e1bd6240b4e8a46668
Richard O'Callaghan <rickojn@hotmail.com>
2019-02-23 10:08:40 +0000

    promises

--- promises.js
+++ promises.js
@@ -1,17 +1,57 @@
 const waitFor = (time) => {
-    return new Promise((resolve, reject) => {
+    return new Promise((resolve) => {
         setTimeout(() => {
+            console.log("in timeout cb");
+            // console.timeEnd("promise2")
             resolve();
-        },time)
+        // },time)
+        },2000)
     });
 };
 
-waitFor(2000).then(() => {
-    console.log('hello');
-});
+// waitFor(2000).then(() => {
+//     console.log('hello');
+// });
 
 // setTimeout(() => {
 //     console.log("hello");
 // }, 2000);
+// console.time("promise");
+// setTimeout(() => {
+//     console.log("hello from callback hell!!!");
+//     setTimeout(() => {
+//         console.log("hello from deeper in callback hell!!!");
+//         setTimeout(() => {
+//             console.log("help I'm totally confused now!!!!!")
+//             console.timeEnd("promise")
+//         }, 1000)
+//     }, 1000);
+// },1000)
 
-setTimeout()
+// console.time("promise");
+// waitFor(1000)
+// .then(() =>{
+//     console.log("hello!");
+// })
+// .then(waitFor(1000))
+// .then( () => {
+//     console.log("hello again!");
+// })
+
+
+console.time("promise");
+// console.time("promise2");
+waitFor(2000)
+.then(() =>{
+    console.log("hello!");
+})
+// .then(() => {return waitFor(2000)})
+.then(waitFor)
+.then( () => {
+    console.log("hello again!");
+    console.timeEnd("promise");   
+})
+// .then(waitFor(5000)).then(() => {
+//     console.log("good bye!!!");
+//     console.timeEnd("promise")
+// });
